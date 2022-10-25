5e40547d8dd5e5e12b67f4c5d279b0cc5fb2442f
Frank Keenan <frank.keenan@oup.com>
2022-10-25 12:33:45 +0100

    Added in eval to do multiple highlights in a cell

--- excel_sensitivity.pl
+++ excel_sensitivity.pl
@@ -5,8 +5,8 @@ use utf8;
 use Excel::Writer::XLSX;
 
 #
-require "/usrdata3/dicts/NEWSTRUCTS/progs/utils.pl";
-require "/NEWdata/dicts/generic/progs/restructure.pl";
+require "./utils.pl";
+require "./restructure.pl";
 #require "/NEWdata/dicts/generic/progs/xsl_lib_fk.pl";
 $LOG = 0;
 $LOAD = 0;
@@ -41,11 +41,12 @@ sub main
     my $unlocked = $workbook->add_format( locked => 0 );
     my $hidden   = $workbook->add_format( hidden => 1 );
     my $red      = $workbook->add_format( color => 'red' );
+    my $black      = $workbook->add_format( color => 'black' );
 
     # Format the columns
-##    $worksheet->set_column( 'A:A', 45, $unlocked );
-##    $worksheet->set_column( 'C:D', 45, $unlocked );
-#    $worksheet->autofilter( 'A1:K1' );
+    ##    $worksheet->set_column( 'A:A', 45, $unlocked );
+    ##    $worksheet->set_column( 'C:D', 45, $unlocked );
+    #    $worksheet->autofilter( 'A1:K1' );
     # Protect the worksheet
     ##    $worksheet->protect({autofilter => 1});
     $worksheet->set_column( 'E:E', 20 );   # Column  E   width set to 20
@@ -79,30 +80,46 @@ sub main
 	$lexid = $FLDS[1];
 	($wd, $context, $hw, $tag, $lexid, $context_cp, $original_text, $pos, $def, $derogatory, $offensive, $vulgar, $sensitivity_classes) = split(/\t/);
 	$worksheet->write_string($row, 0, $wd);
-#	$worksheet->write_string($row, 1, $context);
+	#	$worksheet->write_string($row, 1, $context);       	
 	if ($context =~ m|^(.*?)<red>(.*?)</red>(.*)$|)
 	{
-	    $pre = $1;
-	    $spell = $2;
-	    $end = $3;
-	    $worksheet->write_rich_string($row, 1,  $pre, $red, $spell, $end);
+	    $context = &get_rich_string($context);
+	    $context = sprintf("\$worksheet->write_rich_string( %s, 1, %s)", $row, $context); 
+	    eval $context;
 	}
 	for ($i=2; $i<13; $i++)
 	{
 	    $worksheet->write_string($row, $i, $FLDS[$i]);
 	}
-	    #	$worksheet->write_string($row, 3, $tag);
-#	$worksheet->write_string($row, 4, $lexid);
 	$row++;
-#	$array_ref = \@array;
-#	$worksheet->write_row( $row++, 0, $array_ref );
-#	print $_;
 	if ($opt_O){printf(bugout_fp "%s\n", $_);}
     }
     $workbook->close();
     exit;
     &close_debug_files;
 }
+
+sub get_rich_string
+{
+    my($e) = @_;
+    my($res, $eid);	
+    $e =~ s|(<red>.*?</red>)|&split;&fk;$1&split;|gi;
+    my @BITS = split(/&split;/, $e);
+    my $res = "";
+    foreach my $bit (@BITS){
+	if ($bit =~ s|&fk;||gi){
+	    my $bit = restructure::get_tag_contents($bit, "red"); 
+	    $res = sprintf("%s\$red, \"%s\", ", $res, $bit); 
+	} else {
+	    $res = sprintf("%s\$black, \"%s\", ", $res, $bit); 
+	}
+    }	
+    $res =~ s|, *$||;
+    return $res;
+}
+
+
+
 
 sub usage
 {
