011af8fdf05c8a236b7ac0a25ae22b05b3947f93
Frank Keenan <frank.keenan@oup.com>
2023-09-14 09:16:49 +0100

    Script for Cantonese

--- progs/run_sensitivity_cantonese.sh
+++ progs/run_sensitivity_cantonese.sh
@@ -1,3 +1,6 @@
-perl  getDataForChecking_XML.pl  -f ../Configs/cantonese.cfg Cantonese/jdict2.xml > TMP/dataForChecking.dat
-perl sensitivity_check.pl -f englishSensitiveSmaller.xml TMP/dataForChecking.dat > TMP/sensitiveCantonese.dat
-perl  XLS.pl -r Cantonese_sensitivity.xlsx  TMP/sensitiveCantonese.dat
+echo 'perl  getDataForChecking_XML.pl  -f ../Configs/cantonese.cfg ../Cantonese/dict2.xml > TMP/dataForChecking.dat'
+perl  getDataForChecking_XML.pl  -f ../Configs/cantonese.cfg ../Cantonese/dict2.xml > TMP/dataForChecking.dat
+echo 'perl sensitivity_check_xml.pl -f sensitive_info.xml TMP/dataForChecking.dat | perl add_hw_trans.pl -f TranslationsForHdwdsGoogle.dat | perl add_full_entry.pl -f ../Cantonese/dict2.xml > TMP/sensitiveCantonese.dat'
+perl sensitivity_check_xml.pl -f sensitive_info.xml TMP/dataForChecking.dat | perl add_hw_trans.pl -f TranslationsForHdwdsGoogle.dat | perl add_full_entry.pl -f ../Cantonese/dict2.xml > TMP/sensitiveCantonese.dat
+echo 'perl  XLS_cantonese.pl -r Cantonese_sensitivity.xlsx  TMP/sensitiveCantonese.dat'
+perl  XLS_cantonese.pl -r Cantonese_sensitivity.xlsx  TMP/sensitiveCantonese.dat
